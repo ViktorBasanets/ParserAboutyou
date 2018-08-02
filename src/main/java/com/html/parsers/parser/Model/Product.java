2e5e75ff9b17539712a12e5a2d62bbe8796fb4bd
Viktor Basanets <vbasanec@gmail.com>
2018-08-02 14:13:08 +0300

    Create class of model

---
+++ src/main/java/com/html/parsers/parser/Model/Product.java
@@ -0,0 +1,77 @@
+package com.html.parsers.parser.Model;
+
+public class Product {
+
+    String name;
+    String brand;
+    String color;
+    String price;
+    String initialPrice;
+    String description;
+    String articleId;
+    String shippingCosts;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getInitialPrice() {
+        return initialPrice;
+    }
+
+    public void setInitialPrice(String initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(String articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getShippingCosts() {
+        return shippingCosts;
+    }
+
+    public void setShippingCosts(String shippingCosts) {
+        this.shippingCosts = shippingCosts;
+    }
+}
