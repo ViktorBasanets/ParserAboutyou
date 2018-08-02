4e8713207f132e37867f5b07b07f7d089f7d047f
Viktor Basanets <vbasanec@gmail.com>
2018-08-02 14:01:10 +0300

    Created new project

---
+++ src/main/java/com/html/parsers/parser/Main.java
@@ -0,0 +1,14 @@
+package com.html.parsers.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Runtime.getRuntime();
+    }
+}
